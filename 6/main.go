0240d199690da2566dec14b1215cb7bb3ee42051
Kevin Arnall <kevinjarnall@gmail.com>
2020-12-06 09:19:31 -0600

    add day 6 part 2

--- 6/main.go
+++ 6/main.go
@@ -17,23 +17,40 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	group := make(map[rune]bool)
-	total := 0
+	group := make(map[rune]int)
+	totalP1 := 0
+	peopleInGroup := 0
+	answeredByEveryoneInGroup := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
-			total += len(group)
-			group = make(map[rune]bool)
+			totalP1 += len(group)
+			for _, q := range group {
+				if q == peopleInGroup {
+					answeredByEveryoneInGroup++
+				}
+			}
+
+			group = make(map[rune]int)
+			peopleInGroup = 0
 			continue
 		}
 
+		peopleInGroup++
+
 		for _, letter := range line {
-			group[letter] = true
+			group[letter]++
 		}
 	}
 
-	total += len(group)
+	totalP1 += len(group)
+	for _, q := range group {
+		if q == peopleInGroup {
+			answeredByEveryoneInGroup++
+		}
+	}
 
-	fmt.Printf("Total questions answered: %d\n", total)
+	fmt.Printf("Total questions answered for part 1: %d\n", totalP1)
+	fmt.Printf("Total questions answered for part 2: %d\n", answeredByEveryoneInGroup)
 }
